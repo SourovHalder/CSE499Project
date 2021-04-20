cd0c2c2839b5653ff93e4a4b4a66410cec2a11d1
sourov halder <sourovhalder97@gmail.com>
2021-04-21 00:45:18 +0600

    Update admin_footer.blade.php

--- pharmacy/resources/views/layouts/admin_layout/admin_footer.blade.php
+++ pharmacy/resources/views/layouts/admin_layout/admin_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-  <strong>Copyright &copy; 2020.</strong>
+  <strong>Copyright &copy; 2021.</strong>
   <div class="float-right d-none d-sm-inline-block">
   </div>
 </footer>
